4343759fee411ac9954d151837aab24bb9e73ce2
Sabrine Ayachi <sabrinee.ayachi@gmail.com>
2015-06-03 20:56:48 +0200

    commit

--- src/tr/edu/gsu/nerwip/tools/file/FileNames.java
+++ src/tr/edu/gsu/nerwip/tools/file/FileNames.java
@@ -140,8 +140,6 @@ public class FileNames
 	public final static String FI_UNKNOWN_TYPES = "fb.unknown" + FileNames.EX_TEXT;
 	/** List of location-related adjectives */
 	public final static String FI_DEMONYMS = "demonyms" + FileNames.EX_TEXT;
-	/** Configuration file */
-	public final static String FI_CONFIGURATION = "config" + FileNames.EX_TEXT;
 	
 //	/**
 //	 * Returns the filename used to store the
