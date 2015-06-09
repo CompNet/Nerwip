abd0cee0dc950166474a5a59182857a3d3394f61
Sabrine Ayachi <sabrinee.ayachi@gmail.com>
2015-06-09 12:13:42 +0200

    added Jena Lib for Dbpedia

---
+++ src/tr/edu/gsu/nerwip/tools/dbpedia/DbIdTools.java
@@ -0,0 +1,56 @@
+package tr.edu.gsu.nerwip.tools.dbpedia;
+
+import tr.edu.gsu.nerwip.tools.log.HierarchicalLogger;
+import tr.edu.gsu.nerwip.tools.log.HierarchicalLoggerManager;
+
+import com.hp.hpl.jena.query.*;
+
+/*
+ * Nerwip - Named Entity Extraction in Wikipedia Pages
+
+
+ * Copyright 2011 Yasa Akbulut, Burcu Küpelioğlu & Vincent Labatut
+ * Copyright 2012 Burcu Küpelioğlu, Samet Atdağ & Vincent Labatut
+ * Copyright 2013 Samet Atdağ & Vincent Labatut
+ * Copyright 2014-15 Vincent Labatut
+ * 
+ * This file is part of Nerwip - Named Entity Extraction in Wikipedia Pages.
+ * 
+ * Nerwip - Named Entity Extraction in Wikipedia Pages is free software: you can 
+ * redistribute it and/or modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Nerwip - Named Entity Extraction in Wikipedia Pages is distributed in the hope 
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public 
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Nerwip - Named Entity Extraction in Wikipedia Pages.  
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/**
+ * This class handles DBpedia ids, and more particularly
+ * the mapping between named entities and their DBpedia
+ * ids.
+ * 
+ * @author Sabrine Ayachi
+ * @author Vincent Labatut
+ */
+
+
+public class DbIdTools {
+	
+/////////////////////////////////////////////////////////////////
+//LOGGING			/////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+/** Common object used for logging */
+protected static HierarchicalLogger logger = HierarchicalLoggerManager.getHierarchicalLogger();
+
+
+
+
+}
