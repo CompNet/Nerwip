b45724623ddc891da6e30bfa8031b3d5138bbe7d
Sabrine Ayachi <sabrinee.ayachi@gmail.com>
2015-09-15 11:51:10 +0200

    updated event extraction

--- src/tr/edu/gsu/nerwip/eventextraction/EventExtraction.java
+++ src/tr/edu/gsu/nerwip/eventextraction/EventExtraction.java
@@ -14,6 +14,7 @@ import tr.edu.gsu.nerwip.data.entity.AbstractEntity;
 import tr.edu.gsu.nerwip.data.entity.EntityDate;
 import tr.edu.gsu.nerwip.data.entity.EntityFunction;
 import tr.edu.gsu.nerwip.data.entity.EntityLocation;
+import tr.edu.gsu.nerwip.data.entity.EntityMeeting;
 import tr.edu.gsu.nerwip.data.entity.EntityOrganization;
 import tr.edu.gsu.nerwip.data.entity.EntityPerson;
 import tr.edu.gsu.nerwip.data.entity.EntityProduction;
@@ -49,7 +50,7 @@ import tr.edu.gsu.nerwip.data.entity.EntityType;
  * Class representing the extraction of events.
  * 
  * @author Sabrine Ayachi
- * @author Vincent Labatut
+ * 
  */
 public class EventExtraction {
 	
@@ -59,7 +60,8 @@ public class EventExtraction {
     /** Common object used for logging */
     protected static HierarchicalLogger logger = HierarchicalLoggerManager.getHierarchicalLogger();
 
-   /**
+   
+    /**
     * This method extract events from   
     * article.
     * 
@@ -90,17 +92,17 @@ public class EventExtraction {
 	    {	if(sp>=0)
 	    
 	    {   List<AbstractEntity<?>> le = entities.getEntitiesIn(sp, ep);
-	    	
-	 	    List<AbstractEntity<?>> persons = Entities.filterByType(le,EntityType.PERSON);
+	    	List<AbstractEntity<?>> persons = Entities.filterByType(le,EntityType.PERSON);
 	 		List<AbstractEntity<?>> locations = Entities.filterByType(le,EntityType.LOCATION);
 	 		List<AbstractEntity<?>> orgs = Entities.filterByType(le,EntityType.ORGANIZATION);
 	 		List<AbstractEntity<?>> functs = Entities.filterByType(le,EntityType.FUNCTION);
 	 		List<AbstractEntity<?>> products = Entities.filterByType(le,EntityType.PRODUCTION);
+	 		List<AbstractEntity<?>> meetings = Entities.filterByType(le,EntityType.MEETING);
 	 		List<AbstractEntity<?>> dates = Entities.filterByType(le,EntityType.DATE);
 	 		// only go on if there is at least one person
 			if(!persons.isEmpty())
-			{
-				if(locations.size()>=1 || orgs.size()>=1 || functs.size()>=1 || products.size()>=1)
+			{ 
+				if(locations.size()>=1 || orgs.size()>=1 || functs.size()>=1 || products.size()>=1 || meetings.size()>=1)
 				{   Event event = new Event();
 				    events.add(event);
 				    eventNbr++;
@@ -141,16 +143,14 @@ public class EventExtraction {
 					
 
 				
-				if(orgs.size()==1)
-				{  for(AbstractEntity<?> entity: orgs)
-				{	EntityOrganization org = (EntityOrganization)entity;
-				    event.addOrganization(org);
-				    
-				}
-				}
-				
-				else 
-				{
+				   if(orgs.size()==1)
+				   {  for(AbstractEntity<?> entity: orgs)
+				   {	EntityOrganization org = (EntityOrganization)entity;
+				        event.addOrganization(org);
+				        }
+				   }
+				   else 
+				   {
 					  for (int i=1; i<=orgs.size()-1; i++)
 					  {
 						  Event event1 = new Event();
@@ -167,21 +167,21 @@ public class EventExtraction {
 					    		  +"\"",event1.toString()));
 					  }
 					  
-				}
+				   }
 				
 				
 				
-				if(functs.size()==1)
-				{   for(AbstractEntity<?> entity: functs)
-					{	EntityFunction funct = (EntityFunction)entity;
+				   if(functs.size()==1)
+				   {   for(AbstractEntity<?> entity: functs)
+				   {	EntityFunction funct = (EntityFunction)entity;
 						event.addFunction(funct);
-					}
-				}
-				
-				else 
-				{
-					  for (int i=1; i<=functs.size()-1; i++)
-					  {
+						
+				   }
+				   }
+				   else 
+				   {
+					   for (int i=1; i<=functs.size()-1; i++)
+					   {
 						  Event event1 = new Event();
 					      events.add(event1);
 					      eventNbr++;
@@ -199,15 +199,14 @@ public class EventExtraction {
 				  }
 				
 				
-				if(products.size()==1)
-				{
+				  if(products.size()==1)
+				  {
 					for(AbstractEntity<?> entity: products)
 					{	EntityProduction product = (EntityProduction)entity;
 						event.addProduction(product);
 					}
-				}
-				
-				else 
+				  }
+				  else 
 				{
 					  for (int i=1; i<=products.size()-1; i++)
 					  {
@@ -227,19 +226,45 @@ public class EventExtraction {
 					  
 				}
 				
-				
-				
-				
-				if(dates.size()==1 | dates.size()==2)
-				{
-					for(AbstractEntity<?> entity: dates)
+				  if(meetings.size()==1)
+				  {
+					for(AbstractEntity<?> entity: products)
+					{	EntityMeeting meeting = (EntityMeeting)entity;
+						event.addMeeting(meeting);
+					}
+					
+				  }
+				  else 
+				  {
+					  for (int i=1; i<=meetings.size()-1; i++)
+					  {
+						  Event event1 = new Event();
+					      events.add(event1);
+					      eventNbr++;
+					      for(AbstractEntity<?> entity: persons)
+						  {	EntityPerson person = (EntityPerson)entity;
+							event1.addPerson(person);
+						  }
+					      EntityMeeting meet1 = (EntityMeeting)meetings.get(i);
+					      event1.addMeeting(meet1);
+					      //logger.log("event1 : " + event1.toString());
+					      logger.log(Arrays.asList("Event found for sentence \""+rawText.substring(sp,ep)
+					    		  +"\"",event1.toString()));
+					  }
+					  
+				  }
+				  
+				  if(dates.size()==1 | dates.size()==2)
+				  {
+					  for(AbstractEntity<?> entity: dates)
 					{	EntityDate date = (EntityDate)entity;
 						event.addDate(date);
 					}
-				}
-				
-				else 
-			    {
+					  
+				  }
+				  
+				  else 
+				  {
 					  for (int i=2; i<=dates.size()-1; i++)
 					  {
 						  Event event1 = new Event();
@@ -257,13 +282,11 @@ public class EventExtraction {
 					      
 					  }
 					  
-			    }
-				
-				
-				logger.log(Arrays.asList("Event found for sentence \""+rawText.substring(sp,ep)
+				  }
+				  
+				  logger.log(Arrays.asList("Event found for sentence \""+rawText.substring(sp,ep)
 			    		  +"\"",event.toString()));
-				
-				}
+				  }
 				
 				else
 				logger.log(Arrays.asList("No event found for sentence \""+rawText.substring(sp,ep)));
@@ -273,7 +296,6 @@ public class EventExtraction {
 	    }
 	    
 	    sp = ep;
-	    
 	    
 	    }
 	    logger.decreaseOffset();
@@ -287,7 +309,3 @@ public class EventExtraction {
 	   
 	   
 	   
-   
-	
-	
-	
